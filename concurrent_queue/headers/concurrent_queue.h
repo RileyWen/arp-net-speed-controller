f793576ea8dddff347aa128af5a606f0d6321fe0
Riley Wen <rileywen.work@gmail.com>
2019-09-21 12:12:43 +0800

    Revert "FUCK"
    This reverts commit 34d3cdb4

--- concurrent_queue/headers/concurrent_queue.h
+++ concurrent_queue/headers/concurrent_queue.h
@@ -5,7 +5,7 @@
 #include <mutex>
 #include <condition_variable>
 
-using std::mutex, std::unique_lock, std::condition_variable;
+using std::mutex, std::scoped_lock, std::condition_variable;
 using std::queue;
 
 template<typename T>
@@ -15,47 +15,19 @@ private:
     size_t m_size;
     size_t m_capacity;
     mutable mutex m_mtx;
-    mutable condition_variable m_cv_not_full;
-    mutable condition_variable m_cv_not_empty;
+    condition_variable m_cv_not_full;
+    condition_variable m_cv_not_empty;
 
 public:
     explicit concurrent_queue(size_t capacity) : m_size(0), m_capacity(capacity) {}
 
-    void push_back(T element) {
-        unique_lock<mutex> lk(m_mtx);
+    void push_back(T element);
 
-        while (m_size > m_capacity)
-            m_cv_not_full.wait(lk);
-        m_q.push(element);
+    void pop_front();
 
-        if (m_size == 0)
-            m_cv_not_empty.notify_one();
-    }
+    bool empty() const;
 
-    void pop_front() {
-        unique_lock<mutex> lock(m_mtx);
-        if (m_size > 0) {
-            m_q.pop();
-
-            if (m_size >= m_capacity)
-                m_cv_not_full.notify_one();
-
-            m_size--;
-        }
-    }
-
-    bool empty() const {
-        unique_lock<mutex> lock(m_mtx);
-        return m_size == 0;
-    }
-
-    const T &front() const {
-        unique_lock<mutex> lock(m_mtx);
-        while (m_size == 0)
-            m_cv_not_full.wait(lock);
-
-        return m_q.front();
-    }
+    const T &front() const;
 };
 
 
